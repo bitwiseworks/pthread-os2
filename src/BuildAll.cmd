92c271917647c988f2118998a1bf62e7bde71870
Yuri Dario <yd@os2power.com>
2010-09-29 20:10:31 +0000

    pthread: build as dynamic library, 20100929.
    git-svn-id: http://svn.netlabs.org/repos/ports/pthread/trunk@148 8d6eb4b1-0faa-de11-b929-00e081727c95

--- src/BuildAll.cmd
+++ src/BuildAll.cmd
@@ -1,16 +1,3 @@
-kmk BUILD_TYPE=debug clean
-kmk BUILD_TYPE=release clean
-
-REM
-REM Do not change debug/release order, it is needed to put INSTALL files into
-REM same package.
-REM 
-
-kmk BUILD_TYPE=debug
-kmk BUILD_TYPE=debug BUILD_AOUT=aout
-
-kmk BUILD_TYPE=release
-kmk BUILD_TYPE=release BUILD_AOUT=aout
-
-kmk BUILD_TYPE=debug packing
-kmk BUILD_TYPE=release packing
+kmk clean
+kmk
+kmk packing
