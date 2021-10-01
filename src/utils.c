f1262d59061bea0eafcbdab2d3791e004620b617
Dmitriy Kuminov <coding@dmik.org>
2021-10-01 18:58:50 +0300

    Apply suggestions from code review

--- src/utils.c
+++ src/utils.c
@@ -1,5 +1,3 @@
-
-#include <stdarg.h>
 #define INCL_DOSSEMAPHORES
 #define INCL_DOSMODULEMGR
 #define INCL_DOSMISC
@@ -9,7 +7,10 @@
 
 
 /* Return TRUE iff the region of memory starting at START is
-   readable. */
+   readable.
+
+   Borrowed from kLIBC src/emx/src/os2/utils.c
+   Copyright (c) 1994-1998 by Eberhard Mattes */
 
 int verify_memory (ULONG start, ULONG size)
 {
